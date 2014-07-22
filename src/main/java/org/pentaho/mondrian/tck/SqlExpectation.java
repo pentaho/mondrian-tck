a97497e0532dcfb7c7674ab9ef2d93190d290c3c
Luc Boudreau <lucboudreau@gmail.com>
2014-07-22 11:14:29 -0400

    [BACKLOG-92] Adds helpers for row, type and number of rows validations. Also adds a test for SQL-92 style join support.

--- src/main/java/org/pentaho/mondrian/tck/SqlExpectation.java
+++ src/main/java/org/pentaho/mondrian/tck/SqlExpectation.java
@@ -2,6 +2,7 @@ package org.pentaho.mondrian.tck;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
@@ -26,12 +27,19 @@ public class SqlExpectation {
 
   public void verify( ResultSet rs ) throws Exception {
     // Validate column names
+    validateColumns( rs );
+    // Validate rows
+    validateRows( rs );
+  }
+
+  private void validateColumns( ResultSet rs ) throws Exception {
+    if ( columns == null ) {
+      return;
+    }
     assertEquals(
       "Column names do not correspond to those expected.",
       columns,
       rsToColumns( rs ) );
-    // Validate rows
-    validateRows( rs );
   }
 
   private String[] rsToColumns( ResultSet rs ) throws Exception {
@@ -45,42 +53,85 @@ public class SqlExpectation {
 
   private void validateRows( ResultSet rs ) throws Exception {
     final int nbCols = rs.getMetaData().getColumnCount();
-    if ( rows != null ) {
-      for ( int i = 1; i <= rows.length; i++ ) {
-        final StringBuilder curRow = new StringBuilder();
-        rs.next();
-        for ( int j = 1; j <= nbCols; j++ ) {
-          Object rawValue = rs.getObject( j );
-          validateType( rawValue, types[j] );
-          curRow.append( rawValue.toString() );
+
+    int rowNum = -1;
+    while ( rs.next() ) {
+
+      final StringBuilder curRow = new StringBuilder();
+
+      // Check whether the RS has more rows but we were not expecting more
+      if ( rows != null && ++rowNum >= rows.length && !partial ) {
+        fail( "ResultSet returned more rows than expected" );
+      }
+
+      // Build a string representation of the row
+      for ( int j = 1; j <= nbCols; j++ ) {
+        if ( j > 1 ) {
+          // Add a delimiter
+          curRow.append( "|" );
         }
+
+        final Object rawValue = rs.getObject( j );
+
+        // Validate types
+        if ( types != null ) {
+          validateType( rs.getMetaData().getColumnName( j ), rawValue, types[j - 1] );
+        }
+
+        // Print the value to the buffer.
+        curRow.append( String.valueOf( rawValue.toString() ) );
+      }
+
+      // Now validate that row
+      if ( rows != null ) {
+        assertEquals(
+          "Row content doesn't match.",
+          rows[rowNum],
+          curRow.toString() );
+      } else {
+        // There was no row defined. we bail now.
+        break;
+      }
+
+      // If there are still rows and we're not in partial mode, that's bad
+      if ( !partial && rowNum < ( rows.length - 1 ) ) {
+        fail( "Expected number of rows doesn't match the result." );
+      }
+
+      // Check whether we have reached the limit of the rows to validate
+      if ( partial && rowNum == ( rows.length - 1 ) ) {
+        break;
       }
     }
   }
 
-  private void validateType( Object actual, int expected ) throws Exception {
+  private void validateType( String colName, Object actual, int expected ) throws Exception {
     switch ( expected ) {
       case java.sql.Types.BIGINT:
         assertTrue(
-          "Wrong type for column.",
+          "Wrong type for column " + colName
+          + ", expected type Long but object class was " + actual.getClass().getName(),
           actual.getClass().equals( Long.class ) );
         break;
 
       case java.sql.Types.BOOLEAN:
         assertTrue(
-          "Wrong type for column.",
+          "Wrong type for column " + colName
+          + ", expected type Boolean but object class was " + actual.getClass().getName(),
           actual.getClass().equals( Boolean.class ) );
         break;
 
       case java.sql.Types.INTEGER:
         assertTrue(
-          "Wrong type for column.",
+          "Wrong type for column " + colName
+          + ", expected type Integer but object class was " + actual.getClass().getName(),
           actual.getClass().equals( Integer.class ) );
         break;
 
       case java.sql.Types.VARCHAR:
         assertTrue(
-          "Wrong type for column.",
+          "Wrong type for column " + colName
+          + ", expected type String but object class was " + actual.getClass().getName(),
           actual.getClass().equals( String.class ) );
         break;
 
@@ -103,26 +154,46 @@ public class SqlExpectation {
     private Builder() {
     }
 
+    /**
+     * Sets the SQL query to run.
+     * <p>(mandatory)
+     */
     public Builder query( String query ) {
       this.query = query;
       return this;
     }
 
+    /**
+     * Sets the column names expected
+     * <p>(optional)
+     */
     public Builder columns( String[] columns ) {
       this.columns = columns;
       return this;
     }
 
+    /**
+     * Sets the expected column types. Use values in {@link Types}.
+     * <p>(optional)
+     */
     public Builder types( int[] types ) {
       this.types = types;
       return this;
     }
 
+    /**
+     * Sets the expected rows. The value delimiter is pipe ( '|' ).
+     * <p>(optional)
+     */
     public Builder rows( String[] rows ) {
       this.rows = rows;
       return this;
     }
 
+    /**
+     * Sets whether the rows provided in {@link #rows(String[])} are only the
+     * first rows of the result set and we didn't intend to validate them all.
+     */
     public Builder partial() {
       this.partial  = true;
       return this;
--- src/test/java/org/pentaho/mondrian/tck/JoinTest.java
+++ src/test/java/org/pentaho/mondrian/tck/JoinTest.java
@@ -9,16 +9,61 @@ import org.junit.Test;
 public class JoinTest {
 
   /**
-   * This test verifies that we can join two tables
+   * This test verifies that we can join two tables through the WHERE clause.
    */
   @Test
   public void testOneWayJoin89() throws Exception {
-    SqlExpectation expct =
+    final SqlExpectation expct =
       newBuilder()
+
+        // We join two tables in the WHERE clause and select a column in each
         .query( "select warehouse.warehouse_id, warehouse_class.description from warehouse, warehouse_class where warehouse.warehouse_class_id = warehouse_class.warehouse_class_id" )
+
+        // We expect two columns.
         .columns( new String[] { "warehouse_id", "description" } )
+
+        // We want an integer and a string
         .types( new int[] { Types.INTEGER, Types.VARCHAR } )
+
+        // We will validate the first rows content.
+        .rows(new String[] {
+          "1|Small Independent"
+        } )
+
+        // We won't validate all rows.
+        .partial()
         .build();
+
+    // Validate
+    SqlContext.defaultContext().verify( expct );
+  }
+
+  /**
+   * This test verifies that we can join two tables through the FROM clause.
+   */
+  @Test
+  public void testOneWayJoin92() throws Exception {
+    final SqlExpectation expct =
+      newBuilder()
+
+        // We join two tables in the FROM clause and select a column in each
+        .query( "select warehouse.warehouse_id, warehouse_class.description from warehouse join warehouse_class on (warehouse.warehouse_class_id = warehouse_class.warehouse_class_id)" )
+
+        // We expect two columns.
+        .columns( new String[] { "warehouse_id", "description" } )
+
+        // We want an integer and a string
+        .types( new int[] { Types.INTEGER, Types.VARCHAR } )
+
+        // We will validate the first rows content.
+        .rows(new String[] {
+          "1|Small Independent"
+        } )
+
+        // We won't validate all rows.
+        .partial()
+        .build();
+
     // Validate
     SqlContext.defaultContext().verify( expct );
   }
